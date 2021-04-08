5692efa336559f02a5737b1d57380a0b761c2a1b
Wookie <wookie@kasacorp.com>
2021-04-09 02:36:00 +0900

    Add doc target

---
+++ Server/django-pydoc.py
@@ -0,0 +1,10 @@
+import os
+import django
+import pydoc
+
+# Prepare Django before executing pydoc command
+os.environ['DJANGO_SETTINGS_MODULE'] = 'apiServer.settings' # Change the value according to your django settings path
+django.setup()
+
+# Now executing pydoc
+pydoc.cli()
