7d99a453f849a3db0b0e1ca7a64226ac4dec8c4f
Wookie <wookie@kasacorp.com>
2021-04-09 01:25:41 +0900

    Testing makefile

---
+++ Server/api/urls.py
@@ -0,0 +1,3 @@
+from django.urls import path, include
+from rest_framework import routers
+
---
+++ Server/api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Create your views here.
+
+@api_view(['GET'])
+def get(request):
+    return Response({'message': 'hello'})
